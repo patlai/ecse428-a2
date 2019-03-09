e0051e13e1ff9b33cc46301ea15ac23b92c7855d
Patrick Lai <patricklai10123@gmail.com>
2019-03-09 03:41:14 -0500

    first scenario working

--- src/test/java/com/emailtest/cucumber/StepDefinitions.java
+++ src/test/java/com/emailtest/cucumber/StepDefinitions.java
@@ -1,4 +1,4 @@
-package com.emailtest.cucmber;
+package com.emailtest.cucumber;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,27 +13,28 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 
 import java.awt.*;
-import java.io.File;
+import java.io.*;
 import java.util.concurrent.TimeUnit;
 
 public class StepDefinitions {
 
     private WebDriver driver;
 
-    private final String DRIVER_PATH = "/Users/patricklai/dropbox/u4/ECSE 428/assignment b/driver";
+    private final String DRIVER_PATH = "/Users/patricklai/dropbox/u4/ECSE 428/assignment b/driver/chromedriver";
     private final String OUTLOOK_URL = "https://outlook.live.com/owa/";
-    private final String EMAIL_LOGIN = "test";
-    private final String EMAIL_PASSWORD = "test";
+    private final String EMAIL_LOGIN = "tester113355@outlook.com";
+    private String emailPassword = "";
 
     private final String EMAIL_TEST_SUBJECT = "ECSE 428 Assignment 2 test email";
     private final String NO_RECIPIENT_ERROR = "This message must have at least one recipient.";
+    private final String NO_SUBJECT_WARNING = "Missing subject";
 
     private final String NEW_MESSAGE_CLASS = "ms-Button ms-Button--action ms-Button--command _1Tegvg68M2d-4Z3rDrt2B3 root-50";
 
     private final int DEFAULT_DRIVER_TIMEOUT = 10;
     private final int PAGE_LOAD_TIMEOUT = 2;
 
-    private void startDriver()  {
+    private void startDriver() throws IOException {
         if (driver == null) {
             System.out.println("Starting Chrome driver");
             System.setProperty("webdriver.chrome.driver", DRIVER_PATH);
@@ -43,9 +44,22 @@ public class StepDefinitions {
         else {
             System.out.println("Chrome driver is already running");
         }
+
+        loadPassword();
+    }
+
+    private void loadPassword() throws IOException{
+        File file = new File("password.txt"); 
+
+        if (file.exists()){
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            emailPassword = br.readLine();
+            System.out.println(emailPassword);
+        }
     }
 
     private void waitForPageLoad() throws InterruptedException {
+        System.out.println("waiting for page to load");
         TimeUnit.SECONDS.sleep(PAGE_LOAD_TIMEOUT);
     }
 
@@ -67,43 +81,44 @@ public class StepDefinitions {
     }
 
     @Given("^I am logged in to outlook$")
-    public void givenLoggedIn() throws InterruptedException {
+    public void givenLoggedIn() throws InterruptedException, IOException {
         startDriver();
 
         // navigate to the login page and fill in the username and password fields
         driver.get(OUTLOOK_URL);
-        waitForElementByName("identifier").sendKeys(EMAIL_LOGIN);
-        // tabbing to the next element from the username field should go to the password field
-        waitForElementById("identifierNext").click();
-        waitForElementByName("password").sendKeys(EMAIL_PASSWORD);
-        // go to the login button
-        waitForElementById("passwordNext").click();
+        driver.findElement(By.className("officeHeaderMenu")).click();
+        waitForPageLoad();
+
+        waitForElementById("i0116").sendKeys(EMAIL_LOGIN);
+        waitForElementById("idSIButton9").click();
+        waitForElementById("i0118").sendKeys(emailPassword);
+        waitForElementById("idSIButton9").click();
 
         waitForPageLoad();
         checkLogin();
     }
 
-    @When("^I create a new email$")
+    @When("^I press on New Message$")
     public void createNewEmail(){
-        driver.findElement(By.className(NEW_MESSAGE_CLASS)).click();
+        driver.findElement(By.id("id__5")).click();
     }
 
-    @And("^I add A as a recipient$")
+    @And("^I add recipient A$")
     public void addRecipientA() throws InterruptedException{
-        addRecipient("A");
+        addRecipient("patricklai10123@gmail.com");
     }
 
-    @And("^I add B as a recipient$")
+    @And("^I add recipient B$")
     public void addRecipientB() throws InterruptedException{
         addRecipient("B");
     }
 
-    @And("^I add picture 1$")
+    @And("^I attach image 1$")
     public void addPicture1() throws AWTException, InterruptedException{
         addPicture("picture1.jpg");
     }
 
-    @And("^I add picture 2$")
+    @And("^I attach image 2$")
     public void addPicture2() throws AWTException, InterruptedException{
         addPicture("picture2.jpg");
     }
@@ -113,19 +128,19 @@ public class StepDefinitions {
         driver.findElement(By.id("subjectLine0")).sendKeys(EMAIL_TEST_SUBJECT);
     }
 
-    @And("^I click send$")
+    @And("^I press Send$")
     public void sendEmail() {
         // in the case of outlook's send button, the classname is not always the same,
         // so we need to find the button by matching text
         driver.findElement(By.xpath("//div[text()='Send']")).click();
     }
 
-    @Then("^The email should be sent$")
+    @Then("^the email is sent with an image attached to it$")
     public void checkIfEmailSent() throws InterruptedException{
         waitForPageLoad();
 
         // go to the sent messages
-        driver.findElement(By.xpath("//div[text()='Sent Items']")).click();
+        driver.findElement(By.xpath("//div[@title='Sent Items']")).click();
         new WebDriverWait(driver, DEFAULT_DRIVER_TIMEOUT).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[text()='Empty folder']")));
 
         if (!driver.getPageSource().contains(EMAIL_TEST_SUBJECT)){
@@ -134,7 +149,19 @@ public class StepDefinitions {
         }
     }
 
-    @Then("^An error message is displayed saying there is no recipient$")
+    @Then("^a Missing Subject popup appears asking if you want to send this message without a subject$")
+    public void noSubjectWarningMessage(){
+        if(!driver.getPageSource().contains(NO_SUBJECT_WARNING)){
+            driver.close();
+            throw new Error("No subject warning message was not displayed properly");
+        }
+
+        driver.findElement(By.xpath("//div[text()='Don't send']")).click();
+        checkLogin();
+        driver.close();
+    }
+
+    @Then("^an error message saying This message must have at least one recipient appears$")
     public void noRecipientErrorMessage(){
         if(!driver.getPageSource().contains(NO_RECIPIENT_ERROR)){
             driver.close();
@@ -146,8 +173,9 @@ public class StepDefinitions {
     }
 
     private void addRecipient(String recipient) throws InterruptedException{
-        new WebDriverWait(driver, DEFAULT_DRIVER_TIMEOUT).until(ExpectedConditions.presenceOfElementLocated(By.name("to")));
-        driver.findElement(By.name("to")).sendKeys(recipient);
+        new WebDriverWait(driver, DEFAULT_DRIVER_TIMEOUT)
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@class='ms-BasePicker-input pickerInput_269bfa71']")))
+                .sendKeys(recipient);
     }
 
     private void addPicture(String path) throws InterruptedException, AWTException{
@@ -155,7 +183,13 @@ public class StepDefinitions {
 
         File imageFile = new File(path);
         String imagePath = imageFile.getAbsolutePath();
-        driver.findElement(By.name("Filedata")).sendKeys(imagePath);
+
+        // send the image to the attachment input field
+        driver.findElement(By.xpath("//input[@accept='image/*']")).sendKeys("/Users/patricklai/dropbox/u4/ECSE 428/assignment b/patrick-yueh-a2/image1.jpg");
+
+        // wait for the image to laod
+        waitForPageLoad();
+        //new WebDriverWait(driver, DEFAULT_DRIVER_TIMEOUT).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@dir='ltr']")));
     }
 }
 
